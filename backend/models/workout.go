ac72b57fd4e6812a0fbbe355cb51a238c05b304d
Ferhan0 <akdagferhan9@gmail.com>
2026-03-06 16:18:30 +0300

    UserID type is now uint

--- backend/models/workout.go
+++ backend/models/workout.go
@@ -4,7 +4,7 @@ import "gorm.io/gorm"
 
 type Workout struct {
 	gorm.Model
-	UserID int    `json:"user_id" gorm:"not null"`
+	UserID uint   `json:"user_id" gorm:"not null"`
 	Title  string `json:"title"`
 	Date   string `json:"date"`
 	Notes  string `json:"notes"`
